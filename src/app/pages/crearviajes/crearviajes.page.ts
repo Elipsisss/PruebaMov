84601bed78aee29bb5d9c6abe4e3417f3fe5c9eb
Diego <di.alcaino@duocuc.cl>
2024-10-25 20:45:24 -0300

    crear viajes css tarifa

--- src/app/pages/crearviajes/crearviajes.page.ts
+++ src/app/pages/crearviajes/crearviajes.page.ts
@@ -57,54 +57,66 @@ export class CrearviajesPage implements OnInit, AfterViewInit {
     this.initMap();
   }
 
-  initMap(){
+  initMap() {
     try {
-      //ACA CARGAMOS E INICIALIZAMOS EL MAPA:
-      this.map = L.map("map_html").locate({setView:true, maxZoom:16});
-      //this.map = L.map("map_html").setView([-33.608552227594245, -70.58039819211703],16);
-      
-      //ES LA PLANTILLA PARA QUE SEA VEA EL MAPA:
+      // CARGAMOS E INICIALIZAMOS EL MAPA:
+      this.map = L.map("map_html").locate({ setView: true, maxZoom: 16 });
+  
+      // PLANTILLA PARA QUE SE VEA EL MAPA:
       L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
       }).addTo(this.map);
   
-      this.map.on('locationfound', (e)=>{
+      this.map.on('locationfound', (e) => {
         console.log(e.latlng.lat);
         console.log(e.latlng.lng);
       });
   
-      //VAMOS A AGREGAR UN BUSCADOR DE DIRECCIONES EN EL MAPA:
+      // AGREGAR UN BUSCADOR DE DIRECCIONES EN EL MAPA:
       this.geocoder = G.geocoder({
         placeholder: "Ingrese dirección a buscar",
         errorMessage: "Dirección no encontrada"
       }).addTo(this.map);
-  
-      //VAMOS A REALIZAR UNA ACCIÓN CON EL BUSCADOR, CUANDO OCURRA ALGO CON EL BUSCADOR:
-      this.geocoder.on('markgeocode', (e)=>{
-        //cargo el formulario:
+
+      // ACCIÓN CON EL BUSCADOR, CUANDO OCURRA ALGO CON EL BUSCADOR:
+      this.geocoder.on('markgeocode', (e) => {
+        // CARGO EL FORMULARIO:
         let lat = e.geocode.properties['lat'];
         let lon = e.geocode.properties['lon'];
         this.viaje.controls.nombre_destino.setValue(e.geocode.properties['display_name']);
-        
         this.viaje.controls.id.setValue(uuidv4());
         this.viaje.controls.latitud.setValue(lat);
         this.viaje.controls.longitud.setValue(lon);
-        
-        if(this.map){
+        if (this.map) {
           L.Routing.control({
-            waypoints: [L.latLng(-33.608552227594245, -70.58039819211703),
-              L.latLng(lat,lon)],
-              fitSelectedRoutes: true,
-            }).on('routesfound', (e)=>{
-              this.viaje.controls.distancia_metros.setValue(e.routes[0].summary.totalDistance);
-              this.viaje.controls.tiempo_minutos.setValue(Math.round(e.routes[0].summary.totalTime/60));
+            waypoints: [
+              L.latLng(-33.608552227594245, -70.58039819211703), 
+              L.latLng(lat, lon) 
+            ],
+            fitSelectedRoutes: true,
+            
+          }).on('routesfound', (e) => {
+         
+            /* ASIGNO VARIABLE */
+            const distancia = e.routes[0].summary.totalDistance; 
+            const tiempo = Math.round(e.routes[0].summary.totalTime / 60); 
+
+            /* GUARDO EL TIEMPO Y LA DISTANCIA */
+            this.viaje.controls.distancia_metros.setValue(distancia);
+            this.viaje.controls.tiempo_minutos.setValue(tiempo);
+  
+            // CALCULO
+            const tarifa = this.calcularTarifa(distancia, tiempo * 60); // CONVIERTO EL TIEMPO A SEGUNDOS 
+            this.viaje.controls.valor.setValue(tarifa.toString()); // SI NO LO CONVIERTO A STRING ME SALE UN ERROR XD
           }).addTo(this.map);
         }
       });
     } catch (error) {
+      console.error("Error al inicializar el mapa:", error);
     }
   }
+  
 
 
  async crearViaje() {
@@ -132,21 +144,24 @@ export class CrearviajesPage implements OnInit, AfterViewInit {
   async actualizarViaje() {
     const viajeId = this.viaje.controls.id.value; 
     if (!viajeId) {
-      alert("El ID del viaje no puede estar vacío.");
-      return; 
+        alert("El ID del viaje no puede estar vacío.");
+        return; 
     }
     if (await this.viajeService.updateViaje(viajeId, this.viaje.value)) {
-      alert("Viaje actualizado!");
-      await this.rescatarViajes(); 
+        alert("Viaje actualizado!");
+        await this.rescatarViajes();
+        this.viaje.reset();  
     } else {
-      alert("Error al actualizar el viaje.");
+        alert("No se puede modificar el destino del viaje");
     }
-  }
+}
+
 
   async eliminarViaje(id: string) {
     if (await this.viajeService.deleteViaje(id)) {
       alert("Viaje eliminado!");
-      await this.rescatarViajes(); 
+      await this.rescatarViajes();
+      this.viaje.reset();  
     } else {
       alert("Error al eliminar el viaje.");
     }
@@ -162,6 +177,37 @@ export class CrearviajesPage implements OnInit, AfterViewInit {
     this.usuario = await this.usuarioService.getUsuarioAutenticado();
   }
 
+
+ /*https://practicatest.cl/preguntas/los-taxis-que-usan-taximetro-como-mecanismo-de-cobro-tarifario-deben-exhibir-en-el-parabrisas-delantero-el-valor-de/Z6Ka#:~:text=Explicaci%C3%B3n,(lo%20que%20ocurra%20primero).
+  en base a ese link se hizo el calculo */ 
+  calcularTarifa(distancia: number, tiempo: number): number {
+    const tarifaBase = 300; // Tarifa base para los primeros 200 metros
+    const metros = 200; // 200 metros
+    const incremento = 130; // tarifa que se paga adicional en LOS 60 SEGUNDOS O 200 METROS
+    const tiempo_segundos = 60; // 60 segundos
+  
+    let tarifaTotal = tarifaBase;
+  
+    // Calcular la cantidad de segmentos de 200 metros recorridos
+    if (distancia > metros) {
+      const segmentosDistancia = Math.floor((distancia - metros) / metros);
+      tarifaTotal += segmentosDistancia * incremento;
+    }
+  
+    // Calcular la cantidad de segmentos de 60 segundos
+    if (tiempo > tiempo_segundos) {
+      const segmentosTiempo = Math.floor((tiempo - tiempo_segundos) / tiempo_segundos);
+      tarifaTotal += segmentosTiempo * incremento;
+    }
+  
+    return tarifaTotal; // Retornar el valor de la tarifa total
+  }
+  
+
+  cancelarAccion() {
+    this.viaje.reset(); 
+  }
+  
 
 
 }
