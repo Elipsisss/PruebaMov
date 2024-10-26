ca020ee11dcd474972c47a43d2fa0b17fd0ce386
NWOOD777 <jo.riquelmet@duocuc.cl>
2024-10-26 20:40:08 -0300

    arreglos en pagina dde administration

--- src/app/pages/detalle-reserva/detalle-reserva.page.ts
+++ src/app/pages/detalle-reserva/detalle-reserva.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ViajeService } from 'src/app/services/viaje.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-detalle-reserva',
@@ -10,15 +11,25 @@ import { ViajeService } from 'src/app/services/viaje.service';
 export class DetalleReservaPage implements OnInit {
   reserva: any;
 
-  constructor(private viajeService: ViajeService, private router: Router) {}
+  constructor(
+    private viajeService: ViajeService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   async ngOnInit() {
-    await this.getViajeDetails('id');
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      await this.getViajeDetails(id);
+    } else {
+      console.error('No ID provided in route');
+    }
   }
 
   async getViajeDetails(id: string) {
     try {
       let viaje = await this.viajeService.getViaje(id);
+      this.reserva = viaje;
       console.log(viaje);
     } catch (error) {
       console.error('Error fetching viaje:', error);
